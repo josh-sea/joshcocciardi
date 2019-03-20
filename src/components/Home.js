7ea2cd66b9b2f13c36515602b683c47eb00c4876
josh-sea <joshua.cocciardi@gmail.com>
2019-03-19 20:15:25 -0400

    changes to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Segment, Header } from 'semantic-ui-react'
+import { Segment, Header, Card, Image } from 'semantic-ui-react'
 
 
 const Home = ({}) => (
-      <Segment style={{height: '50vh'}}>
-        <Header as='h2'>
+      <Segment>
+        <Card>
+        <Image src='JoshCocciardi.jpg' size='medium' circular />
+    <Card.Content>
+      <Card.Header>Josh Cocciardi</Card.Header>
+      <Card.Meta>
+        <span className='date'>Full Stack Web Developer, Mechanical Engineer</span>
+      </Card.Meta>
+      <Card.Description>I live in the NYC Metro Area and am looking for a role as a software developer</Card.Description>
+    </Card.Content>
+    </Card>
+        <Header as='h4'>
 
         </Header>
       </Segment>
