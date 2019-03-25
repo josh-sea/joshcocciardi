23ea6585dc6712a7d859bed6b84a11cf0d6341fb
josh-sea <joshua.cocciardi@gmail.com>
2019-03-25 14:27:41 -0400

    commenting and moved style info

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import HeaderBar from './Containers/HeaderBar'
 import Banner from './Containers/Banner'
+
+//#########################################################################
+//toggle urls for local or heroku deployment
 // const BASEURL =  `http://localhost:3000/api/v1/messages`
 const BASEURL =  `https://joshcocciardi-backend.herokuapp.com/api/v1/messages`
+//#########################################################################
+
 
 class App extends Component {
   state = {
@@ -14,12 +19,19 @@ class App extends Component {
     comments: '',
   }
 
+
+//#########################################################################
+//methods for handling hover feature of resume thumbnail on homepage
+
   handleResumeEnter = e => {
     this.setState({resumeHovered: true})
   }
   handleResumeLeave = e => {
     this.setState({resumeHovered: false})
   }
+//#########################################################################
+//#########################################################################
+//post request to backend to post new contact info and message from from using controlled form inputs
 
   handleSubmit = e => {
     e.preventDefault()
@@ -37,18 +49,19 @@ class App extends Component {
         comment: this.state.comments
       })
     })
-    .then(r=>r.json())
-    .then(console.log)
-
-    this.setState({
+    .then(
+      this.setState({
         firstName: '',
         lastName: '',
         email: '',
         comments: ''
       })
-
+    )
     alert('Message Successfully Sent')
   }
+//#########################################################################
+//#########################################################################
+//updated state based on form changes
 
   handleFormText = e => {
     e.target.id==='firstName' && this.setState({firstName: e.target.value})
@@ -56,11 +69,11 @@ class App extends Component {
     e.target.id === 'email' && this.setState({email: e.target.value})
     e.target.id === 'comments' && this.setState({comments: e.target.value})
   }
-
+//#########################################################################
   render() {
     return (
       <Router>
-      <div style={{paddingLeft: '20px', paddingRight: '20px', paddingBottom: '20px', paddingTop: '3px', backgroundColor: '#F0F0DF', overflow: 'hidden'}}>
+      <div style={appStyle}>
         <HeaderBar />
         <Banner
           resumeHovered={this.state.resumeHovered}
@@ -80,3 +93,14 @@ class App extends Component {
 }
 
 export default App;
+
+//#########################################################################
+//css styling for app container
+const appStyle = {
+  paddingLeft: '20px',
+  paddingRight: '20px',
+  paddingBottom: '20px',
+  paddingTop: '3px',
+  backgroundColor: '#F0F0DF',
+  overflow: 'hidden'
+}
